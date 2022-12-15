51ebcce58a2aa86e693e96a169021449edc54fb6
Seal <taseal@corelogic.com>
2022-12-15 13:38:09 -0600

    minor changes to code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,17 @@
 var startBtn = document.querySelector('#quiz-start');
 var landingPage = document.querySelector('.landing-page');
+var question1 = document.querySelector('.question-1');
 
 
-
-function startGame(event) {
+function startQuiz(event) {
     event.preventDefault();
 
     landingPage.style.display = "none";
+    question1.style.display = "flex";
 }
 
+function quizTimer() {
+    
+}
 
-startBtn.addEventListener('click', startGame);
+startBtn.addEventListener('click', startQuiz);
