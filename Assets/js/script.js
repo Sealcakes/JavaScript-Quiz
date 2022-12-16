594900ce08fb59be635e6d3613f6cbed73df3577
Sealcakes <taylorcseal@gmail.com>
2022-12-15 21:20:45 -0600

    added feature to continue to next question after selection answer choice

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,25 +1,26 @@
 var startBtn = document.querySelector('#quiz-start');
 var landingPage = document.querySelector('.landing-page');
 var question1 = document.querySelector('.question-1');
-var question2 = document.querySelector('.question-2')
-var question3 = document.querySelector('.question-3')
-var question4 = document.querySelector('.question-4')
-var question5 = document.querySelector('.question-5')
+var question2 = document.querySelector('.question-2');
+var question3 = document.querySelector('.question-3');
+var question4 = document.querySelector('.question-4');
+var question5 = document.querySelector('.question-5');
 var timerSpanEl = document.querySelector('#timer-number');
 var timerEl = document.querySelector('#timer');
-var scorePage = document.querySelector('.score-report');
+var scoreReport = document.querySelector('.score-report');
 var score = document.querySelector('#score');
-var correctButton = document.querySelector('.correct');
+var correctButton = document.querySelectorAll('.correct');
 var incorrectButtons = document.querySelectorAll('.incorrect');
 
 var secondsLeft = 75;
-
+var currentIndex = 0;
+var questionScreens = [question1, question2, question3, question4, question5];
 
 function startQuiz(event) {
     event.preventDefault();
 
     landingPage.style.display = "none";
-    question1.style.display = "flex";
+    questionScreens[0].style.display = "flex";
     timerEl.style.display = "block";
     quizTimer();
     questions();
@@ -30,27 +31,46 @@ function quizTimer() {
         secondsLeft--;
         timerSpanEl.textContent = secondsLeft;
 
-        if (secondsLeft === 0) {
+        if (secondsLeft <= 0) {
             clearInterval(timerInterval);
-            alert("You are out of time.");
+            endQuiz();
         }
     }, 1000);
 }
 
+function displayQuestion() {
+    questionScreens[currentIndex].style.display = 'none';
+    currentIndex += 1;
+    
+    if (currentIndex == 5) {
+        endQuiz();
+    } else {
+        questionScreens[currentIndex].style.display = 'flex';
+    }
+}
+
 function questions() {
     
-
+    for (i = 0; i < correctButton.length; i++) {
+        correctButton[i].addEventListener('click', function(event) {
+            event.preventDefault();
+            displayQuestion();
+        })
+    }
+    
     for (x = 0; x < incorrectButtons.length; x++) {
-        incorrectButtons[i].addEventListener('click', function(event){
+        incorrectButtons[x].addEventListener('click', function(event){
             event.preventDefault();
             secondsLeft -= 10;
+            displayQuestion();
         })
     }
 }
 
-
-
-function displayScore() {
+function endQuiz() {
+    questionScreens[4].style.display = 'none';
+    scoreReport.style.display = 'flex';
+    clearInterval(timerInterval);
     score.textContent = secondsLeft;
 }
 
