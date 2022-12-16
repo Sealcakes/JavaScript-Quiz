251c16852d9f4c5e1e568f58a508d703898cb901
Sealcakes <taylorcseal@gmail.com>
2022-12-16 14:56:08 -0600

    added local storage for saving user's initials and score

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,4 @@
+// HTML Elements converted to JS Variables
 var startBtn = document.querySelector('#quiz-start');
 var landingPage = document.querySelector('.landing-page');
 var question1 = document.querySelector('.question-1');
@@ -8,16 +9,20 @@ var question5 = document.querySelector('.question-5');
 var timerSpanEl = document.querySelector('#timer-number');
 var timerEl = document.querySelector('#timer');
 var scoreReport = document.querySelector('.score-report');
-var score = document.querySelector('#score');
+var scoreText = document.querySelector('#score');
 var correctButton = document.querySelectorAll('.correct');
 var incorrectButtons = document.querySelectorAll('.incorrect');
-var timerInterval = null;
+var userInitialsInput = document.querySelector('#initials');
+var submitScoreBtn = document.querySelector('#submit-score');
 
-
+// Global Function Variables
 var secondsLeft = 75;
 var currentIndex = 0;
 var questionScreens = [question1, question2, question3, question4, question5];
+var timerInterval = null;
+var score = 0;
 
+// Function to Start Quiz/Timer/Questions after Start Button is clicked
 function startQuiz(event) {
     event.preventDefault();
 
@@ -25,9 +30,10 @@ function startQuiz(event) {
     questionScreens[0].style.display = "flex";
     timerEl.style.display = "block";
     quizTimer();
-    questions();
+    rightOrWrongButton();
 }
 
+// Function to create a timer for quiz, it is started within startQuiz function
 function quizTimer() {
     timerInterval = setInterval(function () {
         secondsLeft--;
@@ -40,18 +46,25 @@ function quizTimer() {
     }, 1000);
 }
 
+/* This function handles hiding/showing quiz questions, uses DOM to access display
+property and either hides or shows display */
 function displayQuestion() {
     questionScreens[currentIndex].style.display = 'none';
     currentIndex += 1;
     
-    if (currentIndex == currentIndex.length) {
+    if (currentIndex == 5) {
         endQuiz();
     } else {
         questionScreens[currentIndex].style.display = 'flex';
     }
 }
 
-function questions() {
+/* Determines if the button the user clicks on to answer the question is right or wrong,
+if the user selects the wrong answer it subtracks 10 seconds from the timer.  Both
+correct and incorrect will advance the quiz to the next question */
+
+// TODO: CREATE CORRECT/INCORRECT INDICATOR HERE
+function rightOrWrongButton() {
     
     for (i = 0; i < correctButton.length; i++) {
         correctButton[i].addEventListener('click', function(event) {
@@ -68,12 +81,26 @@ function questions() {
         })
     }
 }
-
+/* This function displays the end of the quiz screen and shows the user's score */
 function endQuiz() {
     questionScreens[4].style.display = 'none';
     scoreReport.style.display = 'flex';
     clearInterval(timerInterval);
-    score.textContent = secondsLeft;
+    scoreText.textContent = secondsLeft;
+    score = secondsLeft;
 }
 
+// TODO: CREATE SUBMIT SCORE FUNCTION
+function submitScore(event) {
+    event.preventDefault();
+
+    localStorage.setItem('userInitials', userInitialsInput.value);
+    localStorage.setItem('score', score);
+    
+}
+// TODO: LINK SCORE TO HIGHSCORES PAGE
+
+// Event Listener to start quiz
 startBtn.addEventListener('click', startQuiz);
+// Event listener for submit score button
+submitScoreBtn.addEventListener('click', submitScore);
